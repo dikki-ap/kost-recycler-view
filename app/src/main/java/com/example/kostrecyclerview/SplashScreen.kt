5616bbd8909e368fb2de7714ad1a8b6c2a470fb7
Dikki AP <dikki.1710@gmail.com>
2023-03-19 00:27:18 +0700

    Creating Splash Screen

---
+++ app/src/main/java/com/example/kostrecyclerview/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.kostrecyclerview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 100)
+        finish()
+    }
+}
