e737afea1027dce9c0270d47600a0e654c1b7176
Dikki AP <dikki.1710@gmail.com>
2023-03-19 00:57:41 +0700

    Adding About Page

--- app/src/main/java/com/example/kostrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/kostrecyclerview/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.kostrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var about_page : Button
+    private lateinit var rvButton : Button
+
+    private fun initComponents(){
+        about_page = findViewById(R.id.about_page)
+        rvButton = findViewById(R.id.rvButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponents()
+
+        about_page.setOnClickListener {
+            val moveIntent = Intent(this@MainActivity, About::class.java)
+            startActivity(moveIntent)
+        }
+
     }
 }
